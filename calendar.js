b4bc25a7e71fb3b013c792b3788f7932ec6f7204
idannny <dannyivan6@outlook.com>
2022-11-08 22:25:01 -0800

    Automatic month favicon updates

--- calendar.js
+++ calendar.js
@@ -1,13 +1,41 @@
 
 //https://www.sliderrevolution.com/resources/html-calendar/
+// https://www.w3schools.com/js/js_output.asp
 
-function toJan() {
-  favicon.setAttribute("href", "assets/jan.ico"); 
+function toMonth() {
+  const d = new Date();
+  let month = d.getMonth();
+  // let month =1;
+  if(month == 1)
+    favicon.setAttribute("href", "assets/jan.ico"); 
+  else if(month == 2)
+    favicon.setAttribute("href", "assets/feb.ico"); 
+  else if(month == 3)
+    favicon.setAttribute("href", "assets/march.ico"); 
+  else if(month == 4)
+    favicon.setAttribute("href", "assets/april.ico");
+  else if(month == 5)
+    favicon.setAttribute("href", "assets/may.ico"); 
+  else if(month == 6)
+    favicon.setAttribute("href", "assets/june.ico"); 
+  else if(month == 7)
+    favicon.setAttribute("href", "assets/jul.ico");
+  else if(month == 8)
+    favicon.setAttribute("href", "assets/aug.ico"); 
+  else if(month == 9)
+    favicon.setAttribute("href", "assets/sept.ico"); 
+  else if(month == 10)
+    favicon.setAttribute("href", "assets/oct.ico");
+  else if(month == 11)
+    favicon.setAttribute("href", "assets/nov.ico"); 
+  else
+    favicon.setAttribute("href", "assets/dec.ico"); 
 }
 
-function toFeb() {
-  favicon.setAttribute("href", "assets/feb.ico");
-}
+// function toFeb() {
+
+//   favicon.setAttribute("href", "assets/feb.ico");
+// }
 
 var app = {
     settings: {
